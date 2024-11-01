e7d7dd4e03990ed41c1f070cbe45e77741ca3df7
AdemDmrr <97603297+AdemDmrr@users.noreply.github.com>
2024-11-02 00:12:49 +0300

    Add files via upload

---
+++ AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2024 The Android Open Source Project
+# Copyright (C) 2024 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/omni_gts8.mk
+
+COMMON_LUNCH_CHOICES := \
+    omni_gts8-user \
+    omni_gts8-userdebug \
+    omni_gts8-eng
