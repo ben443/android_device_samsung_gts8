aa3dcdb62c55b52301724588d0382ee40b4b228e
GrenJikxd <124490546+GrenJIkxd@users.noreply.github.com>
2025-04-04 18:39:25 -0700

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -5,6 +5,4 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-add_lunch_combo omni_gts8-user
-add_lunch_combo omni_gts8-userdebug
-add_lunch_combo omni_gts8-eng
+COMMON_LUNCH_CHOICES := omni_gts8-eng omni_gts8-userdebug
