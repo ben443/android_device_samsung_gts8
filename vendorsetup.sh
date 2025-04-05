8c701ca3abc380ad75b6554ef967303cb84681b9
GrenJikxd <124490546+GrenJIkxd@users.noreply.github.com>
2025-04-04 19:08:20 -0700

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -4,5 +4,10 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 #
+if [ -z "$COMMON_LUNCH_CHOICES" ]; then
+    export COMMON_LUNCH_CHOICES="some_default_value"
+    echo "COMMON_LUNCH_CHOICES was not set. Setting to default value."
+else
+    echo "COMMON_LUNCH_CHOICES is set to $COMMON_LUNCH_CHOICES"
+fi
 
-COMMON_LUNCH_CHOICES := omni_gts8-eng omni_gts8-userdebug
